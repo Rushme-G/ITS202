885878f0a1bd4624ed2683e07c2666bf29de1d10
Rushme-G <nrtorail@gmail.com>
2020-09-22 17:08:35 +0600

    third commit

---
+++ Lab/arrin.java
@@ -0,0 +1,12 @@
+public class arrin{	
+
+	public static void main(String[] args){
+		String[] list={"sonam","dorji","karma"};
+
+		for (int i=0; i<list.length; i++) {
+			System.out.println(list[i]);	
+
+		}
+	}	
+
+}
